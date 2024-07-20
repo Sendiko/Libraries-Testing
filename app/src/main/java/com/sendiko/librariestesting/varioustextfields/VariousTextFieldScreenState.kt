49529a7cf857ed6d112f0718a2d7f9bf4b5624e5
Sendiko <sarangtawon897@gmail.com>
2024-07-20 20:36:18 +0700

    give default state

--- app/src/main/java/com/sendiko/librariestesting/varioustextfields/VariousTextFieldScreenState.kt
+++ app/src/main/java/com/sendiko/librariestesting/varioustextfields/VariousTextFieldScreenState.kt
@@ -2,6 +2,6 @@ package com.sendiko.librariestesting.varioustextfields
 
 data class VariousTextFieldScreenState(
     val roundedTextFieldText: String = "",
-    val multipleTextFieldText: String = "",
+    val multipleTextFieldText: String = "26",
     val passwordVisible: Boolean = false,
 )
