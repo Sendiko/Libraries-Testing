46de9e42ac30ba3903d2c0b8c9773fcfbd10dd0d
Sendiko <sarangtawon897@gmail.com>
2024-07-21 22:48:56 +0700

    update repo library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,7 +68,7 @@ dependencies {
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.navigation.runtime.ktx)
     implementation(libs.kotlinx.serialization.json)
-    implementation(libs.varioustextfield)
+    implementation(libs.various.textfield)
     implementation(libs.androidx.ui.text.google.fonts)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
