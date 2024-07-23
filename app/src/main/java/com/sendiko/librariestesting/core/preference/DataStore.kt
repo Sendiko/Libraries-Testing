a3d441c848d0b46932088b97a076edc80de101f2
Sendiko <sarangtawon897@gmail.com>
2024-07-23 22:20:14 +0700

    enc(add): dataStore

---
+++ app/src/main/java/com/sendiko/librariestesting/core/preference/DataStore.kt
@@ -0,0 +1,8 @@
+package com.sendiko.librariestesting.core.preference
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore("app_preferences")
